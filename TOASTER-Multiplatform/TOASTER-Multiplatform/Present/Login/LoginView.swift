074cbf1a0ce4ea925813a6fc3b419d8374e7db8c
Genesis <dmsgktn1685@naver.com>
2024-06-05 14:20:49 +0900

    [Feat] 초기 View 세팅

---
+++ TOASTER-Multiplatform/TOASTER-Multiplatform/Present/Login/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginView.swift
+//  TOASTER-Multiplatform
+//
+//  Created by ParkJunHyuk on 6/5/24.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginView()
+}
